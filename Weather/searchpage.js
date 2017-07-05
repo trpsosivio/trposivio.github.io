4d5520118eacc0647156d326f4089ddef42d2f85
trpsosivio <pos13001@byui.edu>
2017-07-05 10:23:07 -0600

    update

--- Weather/searchpage.js
+++ Weather/searchpage.js
@@ -10,33 +10,14 @@ $('#query').keyup(function() {
 				output += '<li>';
 				output += '<a href="//www.wunderground.com' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
 				output += '</li>';
-				$("main").css('display, flex');
 			}
 		}); // end each
 		output += '</ol>';
 		$("#searchResults").html(output); // send results to the page
 	}); // end getJSON
 }); // end onkeyup
-// Get weather data from wunderground.com
-function getData(input) {
-	// Get the data from the wunderground API
-	$.ajax({
-		url: "//api.wunderground.com/api/3f7014209d97ddbd/geolookup/conditions/q/"+ input+".json"
-		, dataType: "jsonp"
-		, success: function (data) {
-			console.log(data);
-			var location = data.location.city + ', ' + data.location.state;
-			var temp_f = data.current_observation.temp_f;
-			console.log('Location is: ' + location);
-			console.log('Temp is: ' + temp_f);
-			$("#cityDisplay").text(location);
-			$("title").html(location + " | Weather Center");
-			$("#currentTemp").html(Math.round(temp_f) + '°');
-			$("#summary").text(toTitleCase(data.current_observation.icon));
-			$("#cover").fadeOut(250);
-		}
-	});
-}
+
+
 
 // Intercept the menu link clicks
 $("#searchResults").on("click", "a", function (evt) {
@@ -44,21 +25,35 @@ $("#searchResults").on("click", "a", function (evt) {
 	// With the text value get the needed value from the weather.json file
 	var jsonCity = $(this).text(); // Franklin, etc...
 	console.log(jsonCity);
-	$.ajax({
-		url: "https//api.wunderground.com/api/3f7014209d97ddbd/geolookup/conditions/q/"+ input+".json"
-		, dataType: "json"
-		, success: function (data) {
-			console.log(data);
-			console.log(data[jsonCity]);
-			var zip = data[jsonCity].zip;
-			console.log(zip);
-			getData(zip);
-		}
-	});
+	index = $(this).index("a");
+	console.log(index);
+	getData(index.lat, index.lon);
+
+
+
+	$("searchResults").hide();
+	document.getElementById('searchResults').style.display = 'none';
+
 });
+function getData(lat, long){
+	$.ajax({
+		url : "https://api.wunderground.com/api/3f7014209d97ddbd/geolookup/conditions/q/"+ lat +","+ long +".json",
+		dataType : "jsonp",
+		success : function(parsed_json) {
+			var location = parsed_json['current_observation']['display_location']['full'];
+			var temp_f = parsed_json['current_observation']['temp_f'];
+			var wind = parsed_json['current_observation']['wind_mph'];
+			var weather = parsed_json['current_observation']['weather'];
+			var feel = parsed_json['current_observation']['feelslike_f'];
+			console.log(parsed_json);
+			var dirwind = parsed_json['current_observation']['wind_dir'];
+			$("#cityDisplay").text(location);
+			$("#currentTemp").html(temp_f + "&#176 F");
+			$("#summary").text(weather)
+			$("#add2").text("Wind Speed: " + wind + " mph");
+			$("#add1").html("Feels like: " + feel + "&#176 F")
+			$("#add3").text("Wind direction: " + dirwind);
 
-
-// A function for changing a string to TitleCase
-function toTitleCase(str){
-	return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+		}
+	})
 }
